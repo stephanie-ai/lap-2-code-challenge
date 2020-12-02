c178a8740951b25232b70a9388764470f39b8698
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-02 12:37:22 +0000

    update username from name

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -17,7 +17,7 @@ class Form extends Component {
 
     handleInputChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.username]: e.target.value
             // username
         });
     }
