037deee2a7fecee76aa152f437c348885e5a55a4
stephanie-ai <stephanie_cooper@hotmail.com>
2020-12-03 15:01:24 +0000

    remove unnecessary code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import FetchGitApi from "../../components/FetchGit"
 import Form from '../../components/Form';
 
 class Home extends Component {
@@ -8,7 +7,6 @@ class Home extends Component {
             <div>
             <h1>Git HUB API</h1>
             <Form />
-            {/* <FetchGitApi /> */}
             </div>
         )
     }
